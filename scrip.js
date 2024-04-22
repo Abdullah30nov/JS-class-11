91819051b2d5439578172b5de467f42a61f67ea0
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-04-22 21:14:46 +0500

    setinterval() & clearInterval()

---
+++ scrip.js
@@ -0,0 +1,10 @@
+// function timer(){
+//     console.log("Seconds");
+// }
+// setTimeout(timer, 1);
+// interval = setInterval(timer,1000)
+
+// function stop(){
+//     clearInterval(interval);
+// }
+
